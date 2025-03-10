1eca3c4dd4908eae45eed0d8869b47665e56355f
Ezequiel Purro <ezequiel.purro@gmail.com>
2025-03-09 21:26:09 -0300

    welcome added

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+    content: [
+      './src/**/*.{html,js,astro}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'solidcat': '#2d1434',
+                'solidcat-light': '#fbf0ff'
+            },
+        },
+    },
+    plugins: [],
+}

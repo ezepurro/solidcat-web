637211a2d24842960d412142b515141a0ee483d8
Santiago Gamond <ssquarkss@gmail.com>
2025-03-16 14:23:33 -0300

    adding languages to the form component

--- src/i18n/get-started/en.ts
+++ src/i18n/get-started/en.ts
@@ -4,5 +4,14 @@ export const en = {
     "title": {
         "title": "Let’s build something great together",
         "subtitle" :"whith the hand of the thecnology everything is possible"
-    }
+    },
+    "form": {
+        "name": "Your name",
+        "email": "Your email",
+        "contact": "Your contact",
+        "title1": "LET'S DEVELOP TOGETHER",
+        "title2": "YOUR NEW IDEA",
+        "placeholder": "What's on your mind?...",
+        "msg": "Send Message"
+}
 }
--- src/i18n/get-started/es.ts
+++ src/i18n/get-started/es.ts
@@ -4,5 +4,14 @@ export const es = {
     "title": {
         "title": "Construyamos algo increíble juntos",
         "subtitle" : "De la mano de la tecnologia, todo es posible"
+    },
+    "form" : {
+        "name" : "Tu nombre",
+        "email" : "Tu email",
+        "contact" : "Tu contacto",
+        "title1" : "DESARROLLEMOS JUNTOS",
+        "title2" : "TU NUEVA IDEA",
+        "placeholder" : "¿Que ronda por tu cabeza?...",
+        "msg" : "Enviar Mensaje"
     }
 }
